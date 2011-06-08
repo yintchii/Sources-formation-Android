d5b3a10ad6b9e9d033741a2e1f92afb3fce69b59
Tarik ALAOUI <tarik@alaoui.me>
2011-06-08 15:43:01 +0200

    Ajout du TP avec sqlite + sdcard

---
+++ TP3 - Sqlite + SDCard/src/com/gemalto/velosud/Station.java
@@ -0,0 +1,67 @@
+package com.gemalto.velosud;
+
+public class Station {
+
+	String Name;
+	String fullAdress;
+	String adress;
+	Double latitude;
+	Double Longitude;
+	int open;
+	int bonus;
+	int number;
+	
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getFullAdress() {
+		return fullAdress;
+	}
+	public void setFullAdress(String fullAdress) {
+		this.fullAdress = fullAdress;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public Double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+	public Double getLongitude() {
+		return Longitude;
+	}
+	public void setLongitude(Double longitude) {
+		Longitude = longitude;
+	}
+	public int getOpen() {
+		return open;
+	}
+	public void setOpen(int open) {
+		this.open = open;
+	}
+	public int getBonus() {
+		return bonus;
+	}
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+	
+	
+}
